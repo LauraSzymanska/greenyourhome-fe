1346efcfa11b697a8d2b2d1245f825394e6d0a7c
Laura Szymańska <lauraszy@wp.pl>
2024-11-13 18:18:25 +0100

    routes definition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {ShellComponent} from '../views/base/shell/shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
