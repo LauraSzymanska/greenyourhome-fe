597a803e74ab81627285a854e488cb44ab141731
Laura Szymańska <lauraszy@wp.pl>
2024-11-21 11:51:35 +0100

    adjusted header to change language

--- src/views/base/header/header.component.ts
+++ src/views/base/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {NgStyle} from '@angular/common';
 import {MatIcon} from '@angular/material/icon';
+import {TranslateService} from '@ngx-translate/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +12,22 @@ import {MatIcon} from '@angular/material/icon';
     MatToolbar,
     NgStyle,
     MatIcon,
+    MatButtonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private translate: TranslateService) {}
 
+  language = signal<string>('');
+
+  ngOnInit(): void {
+    this.language.set(this.translate.getBrowserLang() || this.translate.getDefaultLang());
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+    this.language.set(language);
+  }
 }
